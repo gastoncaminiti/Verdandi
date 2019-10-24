744caa4a5e39eebcee19bfd2634da256db8301dd
gastoncaminiti <gastoncaminiti@outlook.com>
2019-10-24 00:59:25 -0300

    - Código: Naveagación en 8 direcciones.

--- Codigo/Verdandi/manager/LevelManager.gd
+++ Codigo/Verdandi/manager/LevelManager.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 onready var nav : Navigation2D = $Navigation2D
 onready var map : TileMap = $Navigation2D/TileMap
+
 onready var size_cell_x = $Navigation2D/TileMap.cell_size.x / 2
 onready var size_cell_y = $Navigation2D/TileMap.cell_size.y / 2
 
@@ -10,10 +11,10 @@ var pos_unit = Vector2()
 var path : PoolVector2Array
 export var speed := 250
 
-signal units_centered
+signal map_initiated
 
 func _ready():
-	emit_signal("units_centered",size_cell_x, size_cell_y)
+	emit_signal("map_initiated", $Navigation2D/TileMap)
 
 func _input(event: InputEvent):
 	if event is InputEventMouseButton:
@@ -29,19 +30,21 @@ func _process(delta: float) -> void:
 			get_node("Norse-Swordman").move(path[0], (speed * delta)/k)
 		else:
 			path.remove(0)
-			
 #	if get_node("Norse-Swordman").get_distance_to(goal) < 31:
 #		get_node("Norse-Swordman").stop()
 func select_path():
 	
-	pos_unit.x =  stepify(get_node("Norse-Swordman").get_unit_postion().x,size_cell_x) 
-	pos_unit.y =  stepify(get_node("Norse-Swordman").get_unit_postion().y , size_cell_y)
+	#pos_unit.x =  stepify(get_node("Norse-Swordman").get_unit_postion().x,size_cell_x) 
+	#pos_unit.y =  stepify(get_node("Norse-Swordman").get_unit_postion().y , size_cell_y)
 	var node_soldier = get_node("Norse-Swordman")
-	path = nav.get_simple_path( pos_unit, nav.get_closest_point(node_soldier.get_east_postion()), true)
+	print(node_soldier.get_unit_position())
+	path = nav.get_simple_path(node_soldier.get_unit_position(), nav.get_closest_point(node_soldier.get_northeast_position()), true)
 	print(path)
-	var tl = map.map_to_world(map.world_to_map (get_node("Norse-Swordman").get_east_postion()))
-	path[path.size()-1].x = tl.x + size_cell_x
-	path[path.size()-1].y = tl.y + size_cell_y
+	#print(map.world_to_map (get_node("Norse-Swordman").get_east_postion()))
+	var tl = map.map_to_world(map.world_to_map (get_node("Norse-Swordman").get_northeast_position()))
+	#print(map.world_to_map (get_node("Norse-Swordman").get_east_postion()))
+	path[path.size()-1].x = tl.x 
+	path[path.size()-1].y = tl.y + size_cell_y 
 	
 	#path = nav.get_simple_path( pos_unit, nav.get_closest_point(node_soldier.get_north_postion()), true)
 	#if path.size() > 2 and node_soldier.get_distance_to(path[0]) > 10:
@@ -61,4 +64,3 @@ func select_path():
 	#path = nav.get_simple_path( pos_unit, nav.get_closest_point(node_soldier.get_east_postion()), true)
 	#path[path.size()-1].x = stepify(path[path.size()-1].x,size_cell_x) 
 	#path[path.size()-1].y = stepify(path[path.size()-1].y , size_cell_y)
-	pass
--- Codigo/Verdandi/manager/UnitManager.gd
+++ Codigo/Verdandi/manager/UnitManager.gd
@@ -2,17 +2,94 @@ extends KinematicBody2D
 
 # Editable VAR
 export(int) var move_range = 62
-export(int, "North", "South","West","East") var orientation = 0
+export(int) var move_speed = 250
+export(int, "North", "South","West","East","NorthWest","NorthEast","SouthWest","SouthEast") var orientation = 0
 # Private VAR
-
+var my_map:  TileMap
+var my_path: PoolVector2Array
+var size_cell_x
+var size_cell_y 
 
 func _ready():
-	connect_parent_child("units_centered","_goGridCenter")
-	# Configure Coordinate View
+	connect_parent_child("map_initiated","_goMapConfig")
+	# Reposition Elements
+	coordinates_reposition()
+	orientation_reposition()
+
+func get_unit_position():
+	return global_position
+
+func get_north_position():
+	orientation = 0
+	return $North.global_position
+
+func get_south_position():
+	orientation = 1
+	return $South.global_position
+
+func get_west_position():
+	orientation = 2
+	return $West.global_position
+
+func get_east_position():
+	orientation = 3
+	return $East.global_position
+
+func get_northwest_position():
+	orientation = 4
+	return $NorthWest.global_position
+
+func get_northeast_position():
+	orientation = 5
+	return $NorthEast.global_position
+
+func get_distance_to(distance):
+	return global_position.distance_to(distance)
+
+func move(point, speed):
+	orientation_animation()
+	global_position = global_position.linear_interpolate(point, speed)
+	
+func set_animated(name):
+	$AnimatedSprite.play(name)
+
+# Función que permite conectar una señal de nodo padre con una función del nodo hijo.
+func connect_parent_child(nsignal, nfunction):
+	if get_parent().is_in_group("Level"):
+		if get_parent().connect(nsignal,self,nfunction) != OK:
+			print("Error al conectar "+ name +" con el padre - Señal "+nsignal+" Función "+nfunction)
+
+func _goMapConfig(map):
+	my_map = map
+	size_cell_x = my_map.cell_size.x / 2
+	size_cell_y = my_map.cell_size.y / 2
+	global_position = set_center_position_by_cell(get_position_by_cell_index(get_cell_index(global_position)))
+
+func get_cell_index(pos):
+	return my_map.world_to_map(pos)
+
+func get_position_by_cell_index(index):
+	return my_map.map_to_world(index)
+
+func set_center_position_by_cell(pos):
+	pos.y += size_cell_y
+	return pos
+	
+func coordinates_reposition():
 	$North.position.y -= move_range 
 	$South.position.y += move_range
 	$West.position.x  -= move_range
 	$East.position.x  += move_range
+	$NorthWest.position.y -=move_range
+	$NorthWest.position.x -=move_range
+	$NorthEast.position.y -=move_range
+	$NorthEast.position.x +=move_range
+	$SouthWest.position.y +=move_range
+	$SouthWest.position.x -=move_range
+	$SouthEast.position.y +=move_range
+	$SouthEast.position.x +=move_range
+
+func orientation_reposition():
 	match orientation:
 		0:
 			set_animated("idle_north")
@@ -24,30 +101,20 @@ func _ready():
 		3:
 			$AnimatedSprite.flip_h = false
 			set_animated("idle_side")
+		4:
+			$AnimatedSprite.flip_h = true
+			set_animated("idle_diagonal_north")
+		5:
+			$AnimatedSprite.flip_h = false
+			set_animated("idle_diagonal_north")
+		6:
+			$AnimatedSprite.flip_h = true
+			set_animated("idle_diagonal_south")
+		7:
+			$AnimatedSprite.flip_h = false
+			set_animated("idle_diagonal_south")
 
-func get_unit_postion():
-	return global_position
-
-func get_north_postion():
-	orientation = 0
-	return $North.global_position
-
-func get_south_postion():
-	orientation = 1
-	return $South.global_position
-
-func get_west_postion():
-	orientation = 2
-	return $West.global_position
-
-func get_east_postion():
-	orientation = 3
-	return $East.global_position
-
-func get_distance_to(distance):
-	return global_position.distance_to(distance)
-
-func move(point, speed):
+func orientation_animation():
 	match orientation:
 		0:
 			set_animated("walk_north")
@@ -59,20 +126,15 @@ func move(point, speed):
 		3:
 			$AnimatedSprite.flip_h = false
 			set_animated("walk_side")
-	global_position = global_position.linear_interpolate(point, speed)
-	
-func set_animated(name):
-	$AnimatedSprite.play(name)
-
-func stop():
-	set_animated("default")
-	
-# Función que permite conectar una señal de nodo padre con una función del nodo hijo.
-func connect_parent_child(nsignal, nfunction):
-	if get_parent().is_in_group("Level"):
-		if get_parent().connect(nsignal,self,nfunction) != OK:
-			print("Error al conectar "+ name +" con el padre - Señal "+nsignal+" Función "+nfunction)
-
-func _goGridCenter(x,y):
-	position.x =  stepify(global_position.x,x) 
-	position.y =  stepify(global_position.y ,y) -10
+		4:
+			$AnimatedSprite.flip_h = true
+			set_animated("walk_diagonal_north")
+		5:
+			$AnimatedSprite.flip_h = false
+			set_animated("walk_diagonal_north")
+		6:
+			$AnimatedSprite.flip_h = true
+			set_animated("walk_diagonal_south")
+		7:
+			$AnimatedSprite.flip_h = false
+			set_animated("walk_diagonal_south")
