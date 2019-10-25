57a30cc82a9815890209f4d9528a759fe0181aff
gastoncaminiti <gastoncaminiti@outlook.com>
2019-10-24 21:48:22 -0300

    - Código: Brujula Isometrica, Seleccionar camino al recibir señal

--- Codigo/Verdandi/manager/LevelManager.gd
+++ Codigo/Verdandi/manager/LevelManager.gd
@@ -1,17 +1,7 @@
 extends Node2D
 
-onready var nav : Navigation2D = $Navigation2D
-onready var map : TileMap = $Navigation2D/TileMap
-
-onready var size_cell_x = $Navigation2D/TileMap.cell_size.x / 2
-onready var size_cell_y = $Navigation2D/TileMap.cell_size.y / 2
-
-var pos_unit = Vector2()
-
-var path : PoolVector2Array
-export var speed := 250
-
 signal map_initiated
+signal units_moved
 
 func _ready():
 	emit_signal("map_initiated", $Navigation2D/TileMap)
@@ -19,48 +9,6 @@ func _ready():
 func _input(event: InputEvent):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed:
-			select_path()
-			$Line2D.points = PoolVector2Array(path)
+			emit_signal("units_moved", $Navigation2D)
+			$Line2D.points = get_node("Norse-Swordman").get_path()
 			$Line2D.show()
-			
-func _process(delta: float) -> void:
-	if path.size() > 0:
-		var k: float = get_node("Norse-Swordman").get_distance_to(path[0])
-		if k > 10:
-			get_node("Norse-Swordman").move(path[0], (speed * delta)/k)
-		else:
-			path.remove(0)
-#	if get_node("Norse-Swordman").get_distance_to(goal) < 31:
-#		get_node("Norse-Swordman").stop()
-func select_path():
-	
-	#pos_unit.x =  stepify(get_node("Norse-Swordman").get_unit_postion().x,size_cell_x) 
-	#pos_unit.y =  stepify(get_node("Norse-Swordman").get_unit_postion().y , size_cell_y)
-	var node_soldier = get_node("Norse-Swordman")
-	print(node_soldier.get_unit_position())
-	path = nav.get_simple_path(node_soldier.get_unit_position(), nav.get_closest_point(node_soldier.get_northeast_position()), true)
-	print(path)
-	#print(map.world_to_map (get_node("Norse-Swordman").get_east_postion()))
-	var tl = map.map_to_world(map.world_to_map (get_node("Norse-Swordman").get_northeast_position()))
-	#print(map.world_to_map (get_node("Norse-Swordman").get_east_postion()))
-	path[path.size()-1].x = tl.x 
-	path[path.size()-1].y = tl.y + size_cell_y 
-	
-	#path = nav.get_simple_path( pos_unit, nav.get_closest_point(node_soldier.get_north_postion()), true)
-	#if path.size() > 2 and node_soldier.get_distance_to(path[0]) > 10:
-	#	path[path.size()-1].x = stepify(path[path.size()-1].x,size_cell_x) 
-	#	path[path.size()-1].y = stepify(path[path.size()-1].y , size_cell_y)
-	#	return
-	#path = nav.get_simple_path( pos_unit, nav.get_closest_point(node_soldier.get_south_postion()), true)
-	#if path.size() > 2 and node_soldier.get_distance_to(path[0]) > 10:
-	#	path[path.size()-1].x = stepify(path[path.size()-1].x,size_cell_x) 
-	#	path[path.size()-1].y = stepify(path[path.size()-1].y , size_cell_y)
-	#	return
-	#path = nav.get_simple_path( pos_unit, nav.get_closest_point(node_soldier.get_west_postion()), true)
-	#if path.size() > 2 and node_soldier.get_distance_to(path[0]) > 10:
-	#	path[path.size()-1].x = stepify(path[path.size()-1].x,size_cell_x) 
-	#	path[path.size()-1].y = stepify(path[path.size()-1].y , size_cell_y)
-	#	return
-	#path = nav.get_simple_path( pos_unit, nav.get_closest_point(node_soldier.get_east_postion()), true)
-	#path[path.size()-1].x = stepify(path[path.size()-1].x,size_cell_x) 
-	#path[path.size()-1].y = stepify(path[path.size()-1].y , size_cell_y)
--- Codigo/Verdandi/manager/UnitManager.gd
+++ Codigo/Verdandi/manager/UnitManager.gd
@@ -3,22 +3,26 @@ extends KinematicBody2D
 # Editable VAR
 export(int) var move_range = 62
 export(int) var move_speed = 250
+export(int) var cell_range =  1
+export(int) var path_dimension  =  3
 export(int, "North", "South","West","East","NorthWest","NorthEast","SouthWest","SouthEast") var orientation = 0
 # Private VAR
 var my_map:  TileMap
 var my_path: PoolVector2Array
+var my_index_path
 var size_cell_x
 var size_cell_y 
 
 func _ready():
 	connect_parent_child("map_initiated","_goMapConfig")
-	# Reposition Elements
-	coordinates_reposition()
-	orientation_reposition()
+	connect_parent_child("units_moved","_goPahtConfig")
 
 func get_unit_position():
 	return global_position
 
+func get_path():
+	return my_path
+	
 func get_north_position():
 	orientation = 0
 	return $North.global_position
@@ -64,6 +68,32 @@ func _goMapConfig(map):
 	size_cell_x = my_map.cell_size.x / 2
 	size_cell_y = my_map.cell_size.y / 2
 	global_position = set_center_position_by_cell(get_position_by_cell_index(get_cell_index(global_position)))
+	coordinates_reposition()
+	orientation_reposition()
+
+func _goPahtConfig(nav):
+	if(my_path):
+		if(my_index_path < my_path.size()):
+			if(my_path[0] == my_path[1]):
+				if(my_path.size() == 2):
+					orientation_reorientation()
+					set_new_path(nav)
+					print(orientation)
+				else:
+					my_index_path = 2
+			global_position = set_center_position_by_cell(get_position_by_cell_index(get_cell_index(my_path[my_index_path]))) 
+			my_index_path+=1
+		else:
+			set_new_path(nav)
+			orientation_reposition()
+			set_center_path()
+	else:
+		set_new_path(nav)
+		orientation_reposition()
+		set_center_path()
+		global_position = set_center_position_by_cell(get_position_by_cell_index(get_cell_index(my_path[my_index_path]))) 
+	print(my_path)
+	
 
 func get_cell_index(pos):
 	return my_map.world_to_map(pos)
@@ -74,20 +104,38 @@ func get_position_by_cell_index(index):
 func set_center_position_by_cell(pos):
 	pos.y += size_cell_y
 	return pos
+
+func set_center_path():
+	for i in my_path.size():
+		my_path[i] = set_center_position_by_cell(get_position_by_cell_index(get_cell_index(my_path[i])))
+
+func set_new_path(my_nav):
+	my_path = my_nav.get_simple_path(global_position, orientation_global_position(), false)
+	my_index_path = 1
 	
+
 func coordinates_reposition():
-	$North.position.y -= move_range 
-	$South.position.y += move_range
-	$West.position.x  -= move_range
-	$East.position.x  += move_range
-	$NorthWest.position.y -=move_range
-	$NorthWest.position.x -=move_range
-	$NorthEast.position.y -=move_range
-	$NorthEast.position.x +=move_range
-	$SouthWest.position.y +=move_range
-	$SouthWest.position.x -=move_range
-	$SouthEast.position.y +=move_range
-	$SouthEast.position.x +=move_range
+	var index_unit_pos = get_cell_index(global_position)
+	$North.global_position = set_center_position_by_cell(get_position_by_cell_index(Vector2(index_unit_pos.x,index_unit_pos.y -  cell_range)))
+	$South.global_position = set_center_position_by_cell(get_position_by_cell_index(Vector2(index_unit_pos.x,index_unit_pos.y +  cell_range)))
+	$West.global_position  =  set_center_position_by_cell(get_position_by_cell_index(Vector2(index_unit_pos.x - cell_range,index_unit_pos.y)))
+	$East.global_position  =  set_center_position_by_cell(get_position_by_cell_index(Vector2(index_unit_pos.x + cell_range,index_unit_pos.y)))
+	$NorthWest.global_position = set_center_position_by_cell(get_position_by_cell_index(Vector2(index_unit_pos.x - cell_range,index_unit_pos.y - cell_range)))
+	$NorthEast.global_position = set_center_position_by_cell(get_position_by_cell_index(Vector2(index_unit_pos.x + cell_range,index_unit_pos.y - cell_range)))
+	$SouthWest.global_position = set_center_position_by_cell(get_position_by_cell_index(Vector2(index_unit_pos.x - cell_range,index_unit_pos.y + cell_range)))
+	$SouthEast.global_position = set_center_position_by_cell(get_position_by_cell_index(Vector2(index_unit_pos.x + cell_range,index_unit_pos.y + cell_range)))
+	#$North.position.y -= move_range 
+	#$South.position.y += move_range
+	#$West.position.x  -= move_range
+	#$East.position.x  += move_range
+	#$NorthWest.position.y -=move_range
+	#$NorthWest.position.x -=move_range
+	#$NorthEast.position.y -=move_range
+	#$NorthEast.position.x +=move_range
+	#$SouthWest.position.y +=move_range
+	#$SouthWest.position.x -=move_range
+	#$SouthEast.position.y +=move_range
+	#$SouthEast.position.x +=move_range
 
 func orientation_reposition():
 	match orientation:
@@ -138,3 +186,41 @@ func orientation_animation():
 		7:
 			$AnimatedSprite.flip_h = false
 			set_animated("walk_diagonal_south")
+
+func orientation_global_position():
+	match orientation:
+		0:
+			return $North.global_position
+		1:
+			return $South.global_position
+		2:
+			return $West.global_position
+		3:
+			return $East.global_position
+		4:
+			return $NorthWest.global_position
+		5:
+			return $NorthEast.global_position
+		6:
+			return $SouthWest.global_position
+		7:
+			return $SouthEast.global_position
+
+func orientation_reorientation():
+	match orientation:
+		0:
+			orientation = 5
+		1:
+			orientation = 6
+		2:
+			orientation = 4
+		3:
+			orientation = 7
+		4:
+			orientation = 0
+		5:
+			orientation = 3
+		6:
+			orientation = 2
+		7:
+			orientation = 1
