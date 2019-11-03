3e4d5f729e0d0bf766b975f36680af2897466a31
gastoncaminiti <gastoncaminiti@outlook.com>
2019-11-03 17:52:59 -0300

    - Código: Interface Start (Parte II). - Localización: Interface Start.

--- Codigo/Verdandi/manager/InterfaceStartManager.gd
+++ Codigo/Verdandi/manager/InterfaceStartManager.gd
@@ -4,3 +4,12 @@ export(PackedScene)  var open_scene
 
 func _ready():
 	$AnimationPlayer.play("idle")
+
+func _on_LinkButtonPlay_pressed():
+	if(open_scene):
+		return get_tree().change_scene_to(open_scene)
+
+
+func _on_LinkButtonOptions_pressed():
+	print("S")
+	TranslationServer.set_locale("es")
