7328f19aff04a8d7e1dc2acae621a6e01df70aaf
gastoncaminiti <gastoncaminiti@outlook.com>
2019-11-25 11:41:21 -0300

    -Código: Interfaz de runas por alineación. Ajustes en interfaz de siguiente escena. -Datos: Mazos por alineación. -Documentación: Sprint 26

--- Codigo/Verdandi/manager/interface/InterfaceRuneManager.gd
+++ Codigo/Verdandi/manager/interface/InterfaceRuneManager.gd
@@ -1,17 +1,36 @@
 extends Node2D
 
-var runes = null
+var runes_prosperity = null
+var runes_favor = null
+var runes_honor = null
 
 func _ready():
 	hide_rune_view()
-	runes = CardEngine.library()
-	for card in runes.cards():
+	runes_prosperity = CardEngine.library().deck("deck_prosperity")
+	runes_favor = CardEngine.library().deck("deck_favor")
+	runes_honor = CardEngine.library().deck("deck_honor")
+	for card in runes_prosperity.cards():
 		var card_widget  = CardConfig.card_instance()
 		var node_control = CenterContainer.new()
 		card_widget.set_card_data(card)
 		node_control.set_custom_minimum_size(Vector2(160,160))
-		get_node("Layer 2 - GUI/MGrid/Grid").add_child(node_control)
+		get_node("Layer 2 - GUI/MGridProsperity/Grid").add_child(node_control)
 		node_control.add_child(card_widget)
+	for card in runes_favor.cards():
+		var card_widget  = CardConfig.card_instance()
+		var node_control = CenterContainer.new()
+		card_widget.set_card_data(card)
+		node_control.set_custom_minimum_size(Vector2(160,160))
+		get_node("Layer 2 - GUI/MGridFavor/Grid").add_child(node_control)
+		node_control.add_child(card_widget)
+	for card in runes_honor.cards():
+		var card_widget  = CardConfig.card_instance()
+		var node_control = CenterContainer.new()
+		card_widget.set_card_data(card)
+		node_control.set_custom_minimum_size(Vector2(160,160))
+		get_node("Layer 2 - GUI/MGridHonor/Grid").add_child(node_control)
+		node_control.add_child(card_widget)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func set_rune_view(data):
@@ -51,3 +70,21 @@ func hide_rune_view():
 
 func _on_Exit_pressed():
 	return get_tree().change_scene_to(load(ProjectSettings.get_setting("application/run/main_scene")))
+
+
+func _on_LinkProsperity_pressed():
+	get_node("Layer 2 - GUI/MGridProsperity").visible = true
+	get_node("Layer 2 - GUI/MGridFavor").visible = false
+	get_node("Layer 2 - GUI/MGridHonor").visible = false
+
+
+func _on_LinkFavor_pressed():
+	get_node("Layer 2 - GUI/MGridProsperity").visible = false
+	get_node("Layer 2 - GUI/MGridFavor").visible = true
+	get_node("Layer 2 - GUI/MGridHonor").visible = false
+
+
+func _on_LinkHonor_pressed():
+	get_node("Layer 2 - GUI/MGridProsperity").visible = false
+	get_node("Layer 2 - GUI/MGridFavor").visible = false
+	get_node("Layer 2 - GUI/MGridHonor").visible = true
